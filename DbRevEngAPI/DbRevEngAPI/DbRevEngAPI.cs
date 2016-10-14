cda8e81bc6fd7e04aeaff00b0342b4f14ec04ab1
sidegence <sidegence@gmail.com>
2016-10-14 20:46:24 +0100

    reference to dapper; api constructor

--- DbRevEngAPI/DbRevEngAPI/DbRevEngAPI.cs
+++ DbRevEngAPI/DbRevEngAPI/DbRevEngAPI.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace DbRevEngAPI
 {
     public class DbRevEngAPI
     {
+        private readonly IDbConnection _db;
+
+        public DbRevEngAPI(IDbConnection db)
+        {
+            _db = db;
+        }
     }
 }
